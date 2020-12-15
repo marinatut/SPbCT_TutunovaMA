bb2006cdabe66d700783f8775ecb62c735b79e6d
marinatut <74048115+marinatut@users.noreply.github.com>
2020-12-15 20:45:36 +0300

    Add files via upload

---
+++ lab15-16/Lab15/Lab15/MyForm.h
@@ -0,0 +1,213 @@
+#pragma once
+
+namespace Lab15 {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Сводка для MyForm
+	/// </summary>
+	public ref class MyForm : public System::Windows::Forms::Form
+	{
+	public:
+		MyForm(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: добавьте код конструктора
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Освободить все используемые ресурсы.
+		/// </summary>
+		~MyForm()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::Windows::Forms::Label^ label1;
+	private: System::Windows::Forms::Label^ label2;
+	private: System::Windows::Forms::Label^ label3;
+	private: System::Windows::Forms::Label^ label4;
+	private: System::Windows::Forms::TextBox^ textBox1;
+	private: System::Windows::Forms::TextBox^ textBox2;
+	private: System::Windows::Forms::TextBox^ textBox3;
+	private: System::Windows::Forms::TextBox^ textBox4;
+	private: System::Windows::Forms::Button^ button1;
+	private: System::Windows::Forms::Label^ label5;
+	protected:
+
+	private:
+		/// <summary>
+		/// Обязательная переменная конструктора.
+		/// </summary>
+		System::ComponentModel::Container ^components;
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Требуемый метод для поддержки конструктора — не изменяйте 
+		/// содержимое этого метода с помощью редактора кода.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->label1 = (gcnew System::Windows::Forms::Label());
+			this->label2 = (gcnew System::Windows::Forms::Label());
+			this->label3 = (gcnew System::Windows::Forms::Label());
+			this->label4 = (gcnew System::Windows::Forms::Label());
+			this->textBox1 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox2 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox3 = (gcnew System::Windows::Forms::TextBox());
+			this->textBox4 = (gcnew System::Windows::Forms::TextBox());
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->label5 = (gcnew System::Windows::Forms::Label());
+			this->SuspendLayout();
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Location = System::Drawing::Point(13, 28);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(77, 13);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"Километраж: ";
+			// 
+			// label2
+			// 
+			this->label2->AutoSize = true;
+			this->label2->Location = System::Drawing::Point(13, 67);
+			this->label2->Name = L"label2";
+			this->label2->Size = System::Drawing::Size(105, 13);
+			this->label2->TabIndex = 1;
+			this->label2->Text = L"Расход (на 100 км):";
+			// 
+			// label3
+			// 
+			this->label3->AutoSize = true;
+			this->label3->Location = System::Drawing::Point(16, 108);
+			this->label3->Name = L"label3";
+			this->label3->Size = System::Drawing::Size(139, 13);
+			this->label3->TabIndex = 2;
+			this->label3->Text = L"Цена за еденицу топлива:";
+			// 
+			// label4
+			// 
+			this->label4->AutoSize = true;
+			this->label4->Location = System::Drawing::Point(16, 149);
+			this->label4->Name = L"label4";
+			this->label4->Size = System::Drawing::Size(117, 13);
+			this->label4->TabIndex = 3;
+			this->label4->Text = L"Количество поездок: ";
+			// 
+			// textBox1
+			// 
+			this->textBox1->Location = System::Drawing::Point(173, 28);
+			this->textBox1->Name = L"textBox1";
+			this->textBox1->Size = System::Drawing::Size(100, 20);
+			this->textBox1->TabIndex = 4;
+			// 
+			// textBox2
+			// 
+			this->textBox2->Location = System::Drawing::Point(173, 67);
+			this->textBox2->Name = L"textBox2";
+			this->textBox2->Size = System::Drawing::Size(100, 20);
+			this->textBox2->TabIndex = 5;
+			// 
+			// textBox3
+			// 
+			this->textBox3->Location = System::Drawing::Point(173, 105);
+			this->textBox3->Name = L"textBox3";
+			this->textBox3->Size = System::Drawing::Size(100, 20);
+			this->textBox3->TabIndex = 6;
+			// 
+			// textBox4
+			// 
+			this->textBox4->Location = System::Drawing::Point(173, 149);
+			this->textBox4->Name = L"textBox4";
+			this->textBox4->Size = System::Drawing::Size(100, 20);
+			this->textBox4->TabIndex = 7;
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(116, 186);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 8;
+			this->button1->Text = L"Посчитать";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &MyForm::button1_Click);
+			// 
+			// label5
+			// 
+			this->label5->AutoSize = true;
+			this->label5->Location = System::Drawing::Point(13, 221);
+			this->label5->Name = L"label5";
+			this->label5->Size = System::Drawing::Size(0, 13);
+			this->label5->TabIndex = 9;
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(320, 261);
+			this->Controls->Add(this->label5);
+			this->Controls->Add(this->button1);
+			this->Controls->Add(this->textBox4);
+			this->Controls->Add(this->textBox3);
+			this->Controls->Add(this->textBox2);
+			this->Controls->Add(this->textBox1);
+			this->Controls->Add(this->label4);
+			this->Controls->Add(this->label3);
+			this->Controls->Add(this->label2);
+			this->Controls->Add(this->label1);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+			this->PerformLayout();
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^ sender, System::EventArgs^ e) {
+
+		double a, b, c, d, S = 0;
+		a = Convert::ToDouble(textBox1->Text);
+		b = Convert::ToDouble(textBox2->Text);
+		d = Convert::ToDouble(textBox3->Text);
+		c = Convert::ToDouble(textBox4->Text);
+	
+		S = a * (b / 100)*c*d;
+		label5->Text += S;
+
+	}
+private: System::Void textBox1_TextChanged(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e)
+{
+	if (!((e->KeyChar >= '0') && (e->KeyChar <= '9')
+		|| (e->KeyChar == ','))) e->KeyChar = Char(0);
+}
+	private: System::Void textBox2_TextChanged(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e)
+	{
+		if (!((e->KeyChar >= '0') && (e->KeyChar <= '9')
+			|| (e->KeyChar == ','))) e->KeyChar = Char(0);
+	}
+	private: System::Void textBox3_TextChanged(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e)
+	{
+		if (!((e->KeyChar >= '0') && (e->KeyChar <= '9')
+			|| (e->KeyChar == ','))) e->KeyChar = Char(0);
+	}
+
+
+	private: System::Void textBox4_TextChanged(System::Object^ sender, System::Windows::Forms::KeyPressEventArgs^ e)
+	{
+		if (!((e->KeyChar >= '0') && (e->KeyChar <= '9')
+			|| (e->KeyChar == ','))) e->KeyChar = Char(0);
+	}
+};
+}
