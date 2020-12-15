96a5714fc2235c32415ac30482bbe7f6699d7758
marinatut <74048115+marinatut@users.noreply.github.com>
2020-12-15 20:42:28 +0300

    Add files via upload

---
+++ lab13/Lab13_main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "Car.h" 
+#include "Lorry.h"
+using namespace std;
+void f1(Car& c)
+{
+	c.Set_mark("Opel"); 
+	cout << c;
+}
+//функция возвращает объект базового класса как результат
+Lorry f2()
+{
+	Lorry l("Kia", 1, 2, 3); 
+	return l;
+}
+int main()
+{
+	//работа с классом Car 
+	Car a;
+	cin >> a;
+	cout << a;
+	Car b("Ford", 4, 115); 
+	cout << b;
+	a = b;
+	cout << a;
+	//работа с классом Lorry 
+	Lorry c;
+	cin >> c;
+	cout << c; //принцип подстановки
+	f1(c);//передаем объект класса 
+	a=f2();
+	cout<<a;
+
+}
---
+++ lab13/ReadMe.txt
@@ -0,0 +1,32 @@
+Описание класса.
+
+Класс Car:
+торговая марка - string
+число цилиндров - int
+мощность - int
+
+Класс Lorry:
+торговая марка - string
+число цилиндров - int
+мощность - int
+груз - int
+
+Определение компонентных функций.
+
+Класс Car:
+Get_markHlreturn
+Get_cyl
+Get_power
+Set_mark
+Set_cyl
+Set_power
+
+Класс Lorry:
+Get_gruz
+Set_Gruz
+
+Определение глобальных функций.
+
+Глобальные функции в файле Lab13_main.cpp:
+void f1(Car&c)
+Car f2()
