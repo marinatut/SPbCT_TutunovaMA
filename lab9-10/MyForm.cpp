b6a7ef53047918c6d5f982e4dbc31a8d7964717b
marinatut <74048115+marinatut@users.noreply.github.com>
2020-12-15 18:35:53 +0300

    Create MyForm.cpp

---
+++ lab9-10/MyForm.cpp
@@ -0,0 +1,16 @@
+#include "MyForm.h"
+using namespace System;
+using namespace System::Windows::Forms;
+
+[STAThread]
+void Main(cli::array<String^>^ args)
+
+{
+	Application::EnableVisualStyles();
+	Application::SetCompatibleTextRenderingDefault(false);
+
+	Massive::MyForm form;   //project name
+	Application::Run(% form);
+
+
+}
