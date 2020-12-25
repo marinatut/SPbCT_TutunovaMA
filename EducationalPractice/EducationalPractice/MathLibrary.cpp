c830c3976dc6070815be78ba542a62f739b96559
m1xxos <bulynin.misha@gmail.com>
2020-12-25 21:00:57 +0300

    практика

---
+++ EducationalPractice/EducationalPractice/MathLibrary.cpp
@@ -0,0 +1,13 @@
+// MathLibrary.cpp : Defines the exported functions for the DLL.
+//#include "pch.h" // use stdafx.h in Visual Studio 2017 and earlier
+#include "MathLibrary.h"
+
+
+
+void writeTo(const std::vector<char>& data)
+    {
+        std::ofstream filestream("sorted.txt", std::ios::app);
+        copy(data.begin(), data.end(), std::ostream_iterator<char>(filestream, " "));
+        filestream << "\n";
+        filestream.close();
+    }
